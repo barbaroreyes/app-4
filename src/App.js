82631fd6e17a0cb94b2562a816611107240b4602
barbaro <48423441+barbaroreyes@users.noreply.github.com>
2021-07-21 04:31:17 -0500

    added-conection

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import Amplify from 'aws-amplify'
 import config from './aws-exports'
 import {withAuthenticator,AmplifySignOut} from '@aws-amplify/ui-react'
 import './App.css';
-
+Amplify.configure(config)
 function App() {
   return (
     <div className="App">
